144da71e4bcec52488fa3661293e949512e586b5
Artyom <cash.kin.art@gmail.com>
2023-09-27 18:39:52 +0300

    меню Разработка и IT - выпадающее

--- Frilance/src/components/HeaderPublic/Category/DesignCategory/index.jsx
+++ Frilance/src/components/HeaderPublic/Category/DesignCategory/index.jsx
@@ -14,7 +14,6 @@ function DesignCategory({ listItem, left }) {
       // onClick={() => setDesign(false)}
       // onMouseOver={() => setDesign(true)}
       // onMouseOut={() => setDesign(false)}
-
     >
       <Link
         to={`${listItem.design.route}`}
@@ -28,142 +27,138 @@ function DesignCategory({ listItem, left }) {
       </Link>
       {/*<div style={{ opacity: 1, display: design ? 'block' : 'block' }} className={'menubox menubox-all'}>*/}
       <div className={styles.menuBox} style={{ left: `${left}px` }}>
-        <div className={'menulist'}>
-          <div className={'submenu submenu--new'}>
-            {listItem.design.col1 && (
-              <div className={'submenu-column submenu-column--cols-3'}>
-                {listItem.design.col1.map((col1, index) => (
-                  <div key={index}>
-                    <div className={'menutitle'}>
-                      <span className='js-menu-item'>{col1.title}</span>
-                    </div>
-                    <div className={'submenu-group-wrapper'}>
-                      <div className={'submenu-group-inner'}>
-                        {col1.subCategory.map((subCategory, index) => (
-                          <div key={index}>
-                            {/*<Link to={subCategory?.route} className={'submenu-item'}>*/}
-                            <Link to='#' className={'submenu-item'}>
-                              <span className='submenu-item__text js-menu-item' onClick={() => setDesign(false)}>
-                                {subCategory.label}
+        {listItem.design.col1 && (
+          <div className={'submenu-column submenu-column--cols-3'}>
+            {listItem.design.col1.map((col1, index) => (
+              <div key={index}>
+                <div className={'menutitle'}>
+                  <span className='js-menu-item'>{col1.title}</span>
+                </div>
+                <div className={'submenu-group-wrapper'}>
+                  <div className={'submenu-group-inner'}>
+                    {col1.subCategory.map((subCategory, index) => (
+                      <div key={index}>
+                        {/*<Link to={subCategory?.route} className={'submenu-item'}>*/}
+                        <Link to='#' className={'submenu-item'}>
+                          <span className='submenu-item__text js-menu-item' onClick={() => setDesign(false)}>
+                            {subCategory.label}
+                          </span>
+                          {subCategory.icon ? (
+                            <>
+                              <span className='submenu-item__wrap-mark submenu-item__wrap-mark--hot'>
+                                <span
+                                  className='submenu-item__icon tooltipster'
+                                  data-tooltip-text='Очень популярная рубрика'
+                                  data-tooltip-class='pointer-en'
+                                  data-tooltip-thidden='true'>
+                                  <img
+                                    className='submenu-item__icon-img'
+                                    src={`http://192.168.1.56/api/category/icon/${subCategory.icon}`}
+                                    alt='Популярный раздел логотипы'
+                                  />
+                                </span>
                               </span>
-                              {subCategory.icon ? (
-                                <>
-                                  <span className='submenu-item__wrap-mark submenu-item__wrap-mark--hot'>
-                                    <span
-                                      className='submenu-item__icon tooltipster'
-                                      data-tooltip-text='Очень популярная рубрика'
-                                      data-tooltip-class='pointer-en'
-                                      data-tooltip-thidden='true'>
-                                      <img
-                                        className='submenu-item__icon-img'
-                                        src={`http://192.168.1.56/api/category/icon/${subCategory.icon}`}
-                                        alt='Популярный раздел логотипы'
-                                      />
-                                    </span>
-                                  </span>
-                                </>
-                              ) : (
-                                <></>
-                              )}
-                            </Link>
-                          </div>
-                        ))}
+                            </>
+                          ) : (
+                            <></>
+                          )}
+                        </Link>
                       </div>
-                    </div>
+                    ))}
                   </div>
-                ))}
+                </div>
               </div>
-            )}
-            {listItem.design.col2 && (
-              <div className={'submenu-column submenu-column--cols-3'}>
-                {listItem.design.col2.map((col2, index) => (
-                  <div key={index}>
-                    <div className={'menutitle'}>
-                      <span className='js-menu-item'>{col2.title}</span>
-                    </div>
-                    <div className={'submenu-group-wrapper'}>
-                      <div className={'submenu-group-inner'}>
-                        {col2.subCategory.map((subCategory, index) => (
-                          <div key={index}>
-                            {/*<Link to={subCategory?.route} className={'submenu-item'}>*/}
-                            <Link to='#' className={'submenu-item'}>
-                              <span className='submenu-item__text js-menu-item' onClick={() => setDesign(false)}>
-                                {subCategory.label}
-                              </span>
-                              {subCategory.icon ? (
-                                <>
-                                  <span className='submenu-item__wrap-mark submenu-item__wrap-mark--hot'>
-                                    <span
-                                      className='submenu-item__icon tooltipster'
-                                      data-tooltip-text='Очень популярная рубрика'
-                                      data-tooltip-class='pointer-en'
-                                      data-tooltip-thidden='true'>
-                                      <img
-                                        className='submenu-item__icon-img'
-                                        src={`http://192.168.1.56/api/category/icon/${subCategory.icon}`}
-                                        alt='Популярный раздел логотипы'
-                                      />
-                                    </span>
-                                  </span>
-                                </>
-                              ) : (
-                                <></>
-                              )}
-                            </Link>
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            )}
-            {listItem.design.col3 && (
-              <div className={'submenu-column submenu-column--cols-3'}>
-                {listItem.design.col3.map((col3, index) => (
-                  <div key={index}>
-                    <div className={'menutitle'}>
-                      <span className='js-menu-item'>{col3.title}</span>
-                    </div>
-                    <div className={'submenu-group-wrapper'}>
-                      <div className={'submenu-group-inner'}>
-                        {col3.subCategory.map((subCategory, index) => (
-                          <div key={index}>
-                            {/*<Link to={subCategory?.route} className={'submenu-item'}>*/}
-                            <Link to='#' className={'submenu-item'}>
-                              <span className='submenu-item__text js-menu-item' onClick={() => setDesign(false)}>
-                                {subCategory.label}
-                              </span>
-                              {subCategory.icon ? (
-                                <>
-                                  <span className='submenu-item__wrap-mark submenu-item__wrap-mark--hot'>
-                                    <span
-                                      className='submenu-item__icon tooltipster'
-                                      data-tooltip-text='Очень популярная рубрика'
-                                      data-tooltip-class='pointer-en'
-                                      data-tooltip-thidden='true'>
-                                      <img
-                                        className='submenu-item__icon-img'
-                                        src={`http://192.168.1.56/api/category/icon/${subCategory.icon}`}
-                                        alt='Популярный раздел логотипы'
-                                      />
-                                    </span>
-                                  </span>
-                                </>
-                              ) : (
-                                <></>
-                              )}
-                            </Link>
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            )}
+            ))}
           </div>
-        </div>
+        )}
+        {listItem.design.col2 && (
+          <div className={'submenu-column submenu-column--cols-3'}>
+            {listItem.design.col2.map((col2, index) => (
+              <div key={index}>
+                <div className={'menutitle'}>
+                  <span className='js-menu-item'>{col2.title}</span>
+                </div>
+                <div className={'submenu-group-wrapper'}>
+                  <div className={'submenu-group-inner'}>
+                    {col2.subCategory.map((subCategory, index) => (
+                      <div key={index}>
+                        {/*<Link to={subCategory?.route} className={'submenu-item'}>*/}
+                        <Link to='#' className={'submenu-item'}>
+                          <span className='submenu-item__text js-menu-item' onClick={() => setDesign(false)}>
+                            {subCategory.label}
+                          </span>
+                          {subCategory.icon ? (
+                            <>
+                              <span className='submenu-item__wrap-mark submenu-item__wrap-mark--hot'>
+                                <span
+                                  className='submenu-item__icon tooltipster'
+                                  data-tooltip-text='Очень популярная рубрика'
+                                  data-tooltip-class='pointer-en'
+                                  data-tooltip-thidden='true'>
+                                  <img
+                                    className='submenu-item__icon-img'
+                                    src={`http://192.168.1.56/api/category/icon/${subCategory.icon}`}
+                                    alt='Популярный раздел логотипы'
+                                  />
+                                </span>
+                              </span>
+                            </>
+                          ) : (
+                            <></>
+                          )}
+                        </Link>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+        {listItem.design.col3 && (
+          <div className={'submenu-column submenu-column--cols-3'}>
+            {listItem.design.col3.map((col3, index) => (
+              <div key={index}>
+                <div className={'menutitle'}>
+                  <span className='js-menu-item'>{col3.title}</span>
+                </div>
+                <div className={'submenu-group-wrapper'}>
+                  <div className={'submenu-group-inner'}>
+                    {col3.subCategory.map((subCategory, index) => (
+                      <div key={index}>
+                        {/*<Link to={subCategory?.route} className={'submenu-item'}>*/}
+                        <Link to='#' className={'submenu-item'}>
+                          <span className='submenu-item__text js-menu-item' onClick={() => setDesign(false)}>
+                            {subCategory.label}
+                          </span>
+                          {subCategory.icon ? (
+                            <>
+                              <span className='submenu-item__wrap-mark submenu-item__wrap-mark--hot'>
+                                <span
+                                  className='submenu-item__icon tooltipster'
+                                  data-tooltip-text='Очень популярная рубрика'
+                                  data-tooltip-class='pointer-en'
+                                  data-tooltip-thidden='true'>
+                                  <img
+                                    className='submenu-item__icon-img'
+                                    src={`http://192.168.1.56/api/category/icon/${subCategory.icon}`}
+                                    alt='Популярный раздел логотипы'
+                                  />
+                                </span>
+                              </span>
+                            </>
+                          ) : (
+                            <></>
+                          )}
+                        </Link>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
--- Frilance/src/components/HeaderPublic/Category/ItCategory/index.jsx
+++ Frilance/src/components/HeaderPublic/Category/ItCategory/index.jsx
@@ -20,142 +20,87 @@ function ItCategory({ listItem, left }) {
         <span className={styles.itemMenu}>{listItem.it.label}</span>
       </Link>
       <div className={styles.menuBox} style={{ left: `${left}px` }}>
-        <div className={'menulist'}>
-          <div className={'submenu submenu--new'}>
-            {listItem.it.col1 && (
-              <div className={'submenu-column submenu-column--cols-3'}>
-                {listItem.it.col1.map((col1, index) => (
+        {listItem.it.col1 && (
+          <div className={styles.firstMenuBox}>
+            {listItem.it.col1.map((col1, index) => (
+              <div className={styles.linkBox} key={index}>
+                <h4 className={styles.titleDropMenu}>{col1.title}</h4>
+                {col1.subCategory.map((subCategory, index) => (
                   <div key={index}>
-                    <div className={'menutitle'}>
-                      <span className='js-menu-item'>{col1.title}</span>
-                    </div>
-                    <div className={'submenu-group-wrapper'}>
-                      <div className={'submenu-group-inner'}>
-                        {col1.subCategory.map((subCategory, index) => (
-                          <div key={index}>
-                            {/*<Link to={subCategory?.route} className={'submenu-item'}>*/}
-                            <Link to='#' className={'submenu-item'}>
-                              <span className='submenu-item__text js-menu-item' onClick={() => setIt(false)}>
-                                {subCategory.label}
-                              </span>
-                              {subCategory.icon ? (
-                                <>
-                                  <span className='submenu-item__wrap-mark submenu-item__wrap-mark--hot'>
-                                    <span
-                                      className='submenu-item__icon tooltipster'
-                                      data-tooltip-text='Очень популярная рубрика'
-                                      data-tooltip-class='pointer-en'
-                                      data-tooltip-thidden='true'>
-                                      <img
-                                        className='submenu-item__icon-img'
-                                        src='https://cdn-edge.kwork.ru/images/mobile_menu/fire.svg'
-                                        alt='Популярный раздел логотипы'
-                                      />
-                                    </span>
-                                  </span>
-                                </>
-                              ) : (
-                                <></>
-                              )}
-                            </Link>
-                          </div>
-                        ))}
-                      </div>
-                    </div>
+                    <Link className={styles.submenuItem} to='#'>
+                      <span className={styles.menuDesc}>{subCategory.label}</span>
+                      {subCategory.icon && (
+                        <img
+                          data-tooltip-text='Очень популярная рубрика'
+                          width={15}
+                          height={15}
+                          src='https://cdn-edge.kwork.ru/images/mobile_menu/fire.svg'
+                          alt='Популярный раздел логотипы'
+                        />
+                      )}
+                    </Link>
                   </div>
                 ))}
               </div>
-            )}
-            {listItem.it.col2 && (
-              <div className={'submenu-column submenu-column--cols-3'}>
-                {listItem.it.col2.map((col2, index) => (
-                  <div key={index}>
-                    <div className={'menutitle'}>
-                      <span className='js-menu-item'>{col2.title}</span>
-                    </div>
-                    <div className={'submenu-group-wrapper'}>
-                      <div className={'submenu-group-inner'}>
-                        {col2.subCategory.map((subCategory, index) => (
-                          <div key={index}>
-                            {/*<Link to={subCategory?.route} className={'submenu-item'}>*/}
-                            <Link to='#' className={'submenu-item'}>
-                              <span className='submenu-item__text js-menu-item' onClick={() => setIt(false)}>
-                                {subCategory.label}
-                              </span>
-                              {subCategory.icon ? (
-                                <>
-                                  <span className='submenu-item__wrap-mark submenu-item__wrap-mark--hot'>
-                                    <span
-                                      className='submenu-item__icon tooltipster'
-                                      data-tooltip-text='Очень популярная рубрика'
-                                      data-tooltip-class='pointer-en'
-                                      data-tooltip-thidden='true'>
-                                      <img
-                                        className='submenu-item__icon-img'
-                                        src='https://cdn-edge.kwork.ru/images/mobile_menu/fire.svg'
-                                        alt='Популярный раздел логотипы'
-                                      />
-                                    </span>
-                                  </span>
-                                </>
-                              ) : (
-                                <></>
-                              )}
-                            </Link>
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            )}
-            {listItem?.it.col3 && (
-              <div className={'submenu-column submenu-column--cols-3'}>
-                {listItem.it.col3.map((col3, index) => (
-                  <div key={index}>
-                    <div className={'menutitle'}>
-                      <span className='js-menu-item'>{col3.title}</span>
-                    </div>
-                    <div className={'submenu-group-wrapper'}>
-                      <div className={'submenu-group-inner'}>
-                        {col3.subCategory.map((subCategory, index) => (
-                          <div key={index}>
-                            {/*<Link to={subCategory?.route} className={'submenu-item'}>*/}
-                            <Link to='#' className={'submenu-item'}>
-                              <span className='submenu-item__text js-menu-item' onClick={() => setIt(false)}>
-                                {subCategory.label}
-                              </span>
-                              {subCategory.icon ? (
-                                <>
-                                  <span className='submenu-item__wrap-mark submenu-item__wrap-mark--hot'>
-                                    <span
-                                      className='submenu-item__icon tooltipster'
-                                      data-tooltip-text='Очень популярная рубрика'
-                                      data-tooltip-class='pointer-en'
-                                      data-tooltip-thidden='true'>
-                                      <img
-                                        className='submenu-item__icon-img'
-                                        src='https://cdn-edge.kwork.ru/images/mobile_menu/fire.svg'
-                                        alt='Популярный раздел логотипы'
-                                      />
-                                    </span>
-                                  </span>
-                                </>
-                              ) : (
-                                <></>
-                              )}
-                            </Link>
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            )}
+            ))}
           </div>
-        </div>
+        )}
+        {listItem.it.col2 && (
+          <div className={styles.firstMenuBox}>
+            {listItem.it.col2.map((col2, index) => (
+              <div className={styles.linkBox} key={index}>
+                <h4 className={styles.titleDropMenu}>{col2.title}</h4>
+                {col2.subCategory.map((subCategory, index) => (
+                  <div key={index}>
+                    <Link to='#' className={styles.submenuItem}>
+                      <span className={styles.menuDesc}>{subCategory.label}</span>
+                      {subCategory.icon && (
+                        <img
+                          width={15}
+                          height={15}
+                          data-tooltip-text='Очень популярная рубрика'
+                          data-tooltip-class='pointer-en'
+                          data-tooltip-thidden='true'
+                          className='submenu-item__icon-img'
+                          src='https://cdn-edge.kwork.ru/images/mobile_menu/fire.svg'
+                          alt='Популярный раздел логотипы'
+                        />
+                      )}
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        )}
+        {listItem?.it.col3 && (
+          <div className={styles.firstMenuBox}>
+            {listItem.it.col3.map((col3, index) => (
+              <div className={styles.linkBox} key={index}>
+                <h4 className={styles.titleDropMenu}>{col3.title}</h4>
+                {col3.subCategory.map((subCategory, index) => (
+                  <div key={index}>
+                    {/*<Link to={subCategory?.route} className={'submenu-item'}>*/}
+                    <Link className={styles.submenuItem} to='#'>
+                      <span className={styles.menuDesc}>{subCategory.label}</span>
+                      {subCategory.icon && (
+                        <img
+                          width={15}
+                          height={15}
+                          data-tooltip-text='Очень популярная рубрика'
+                          data-tooltip-class='pointer-en'
+                          data-tooltip-thidden='true'
+                          src='https://cdn-edge.kwork.ru/images/mobile_menu/fire.svg'
+                          alt='Популярный раздел логотипы'
+                        />
+                      )}
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
